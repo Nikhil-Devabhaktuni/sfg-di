d9f1365a192ec2b059ca849f0e8afc7c3f384875
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-19 21:41:36 +0100

    Removed Default profile annotation
    Just removed the default annotation from dog service class and also included comments in apploication.properties file

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"dog", "default"})
+@Profile("dog")
 @Service
 public class DogPetService implements PetService{
 
--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/27/19.
  */
-@Profile({"ES", "default"})
+@Profile("ES")
 @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
